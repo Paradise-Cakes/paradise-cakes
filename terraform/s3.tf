f7e1f176d43a34595cec767b9ea456282b526b76
Anthony Viera <anthonyviera4@gmail.com>
2023-10-13 23:21:29 -0500

    plz boss

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -9,9 +9,19 @@ resource "aws_s3_bucket" "paradise_cakes_bucket" {
   }
 }
 
-resource "aws_s3_bucket_policy" "paradise_cakes_s3_policy" {
+resource "aws_s3_bucket_public_access_block" "paradise_cakes_bucket_pab" {
   bucket = aws_s3_bucket.paradise_cakes_bucket.id
-  policy = data.aws_iam_policy_document.paradise_cakes_s3_policy.json
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "paradise_cakes_s3_policy" {
+  bucket     = aws_s3_bucket.paradise_cakes_bucket.id
+  policy     = data.aws_iam_policy_document.paradise_cakes_s3_policy.json
+  depends_on = [aws_s3_bucket_public_access_block.paradise_cakes_bucket_pab]
 }
 
 data "aws_iam_policy_document" "paradise_cakes_s3_policy" {
