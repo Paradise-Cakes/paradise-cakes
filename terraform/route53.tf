ffb2e6c27799e691a3bc4cc0666cb7bec2255bcf
Anthony Viera <anthonyviera4@gmail.com>
2023-10-13 21:08:05 -0500

    Now

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -7,7 +7,7 @@ resource "aws_route53_record" "paradise_cakes_record" {
   zone_id = data.aws_route53_zone.zone.zone_id
   name    = "paradisecakesbymegan.com"
   type    = "A"
-  alias = {
+  alias {
     name                   = aws_cloudfront_distribution.pc_cloud_distribution.domain_name
     zone_id                = aws_cloudfront_distribution.pc_cloud_distribution.hosted_zone_id
     evaluate_target_health = false
