48a632480e4747b05dc4be9eb45893378ee935f0
Anthony Viera <anthonyviera4@gmail.com>
2023-10-15 22:56:03 -0500

    i be back

--- src/navigation/Navbar.jsx
+++ src/navigation/Navbar.jsx
@@ -9,20 +9,37 @@ import {
   MenuItem,
   Toolbar,
   Typography,
+  Hidden,
 } from "@mui/material";
+import { CgMenu } from "react-icons/cg";
 
 function Navbar() {
   return (
     <AppBar position="static" sx={{ backgroundColor: "#CDCBBC" }}>
       <Container maxWidth={"false"} sx={{ margin: 0, width: "100%" }}>
         <Toolbar disableGutters sx={{ width: "100%" }}>
-          <img src={AppBarLogo} width="120px" style={{ marginTop: "4px" }} />
-          <Box sx={{ marginLeft: "auto", marginRight: "auto" }}>
-            <Button sx={{ color: "#fff" }}>Cakes</Button>
-            <Button sx={{ color: "#fff" }}>Cupcakes</Button>
-            <Button sx={{ color: "#fff" }}>Flavors</Button>
-            <Button sx={{ color: "#fff" }}>All Products</Button>
-          </Box>
+          <Hidden xsDown lgUp>
+            <img
+              src={AppBarLogo}
+              width="120px"
+              style={{
+                marginTop: "4px",
+                cursor: "pointer",
+                userSelect: "none",
+              }}
+            />
+            <Box sx={{ marginLeft: "auto", marginRight: "auto" }}>
+              <Button sx={{ color: "#fff" }}>Cakes</Button>
+              <Button sx={{ color: "#fff" }}>Cupcakes</Button>
+              <Button sx={{ color: "#fff" }}>Flavors</Button>
+              <Button sx={{ color: "#fff" }}>All Products</Button>
+            </Box>
+          </Hidden>
+          <Hidden smUp>
+            <CgMenu
+              style={{ width: "25px", height: "25px", cursor: "pointer" }}
+            />
+          </Hidden>
         </Toolbar>
       </Container>
     </AppBar>
