f53bb8d11294f37b059ab2736ca0ef7bb388032c
Anthony Viera <anthonyviera4@gmail.com>
2023-10-24 20:16:20 -0500

    I like this

--- src/components/shop/Shop.jsx
+++ src/components/shop/Shop.jsx
@@ -5,95 +5,115 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import { CardActionArea } from "@mui/material";
 import Grid from "@mui/material/Grid";
+import { fontWeight } from "@mui/system";
 
 export default function Shop() {
   return (
     <Grid
       container
-      direction={"column"}
       style={{
-        paddingTop: "132px",
-        paddingLeft: "64px",
-        paddingRight: "64px",
+        paddingTop: "256px",
+        paddingBottom: "256px",
+        paddingLeft: "256px",
+        paddingRight: "256px",
         width: "100%",
       }}
+      spacing={5}
+      justifyContent={"center"}
     >
-      <Grid item>
+      <Grid item sx={{ width: "250px" }}>
         <Typography
           variant="h4"
           fontWeight={1000}
-          sx={{ textAlign: "center", paddingBottom: "16px" }}
+          sx={{
+            textAlign: "center",
+            paddingBottom: "8px",
+            paddingTop: "24px",
+            textAlign: "left",
+          }}
         >
           CAKES
         </Typography>
+        <Typography sx={{ textAlign: "left" }}>
+          Indulge in my delectable selection of freshly baked cakes, made with
+          the finest ingredients and crafted with care.
+        </Typography>
       </Grid>
-      <Grid
-        item
-        container
-        alignItems="center"
-        justifyContent="space-between"
-        spacing={5}
-      >
-        <Grid item container justifyContent="center" xl={3} lg={4} md={6}>
-          <Card sx={{ maxWidth: 500, borderRadius: "12px" }}>
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                image="https://place-hold.it/500/666"
-              />
-
-              <CardContent
-                sx={{
-                  fontWeight: "800",
-                  fontSize: "1rem",
-                  textAlign: "center",
-                }}
+      <Grid item>
+        <Card sx={{ maxWidth: 500, borderRadius: "12px" }}>
+          <CardActionArea>
+            <CardMedia component="img" image="https://place-hold.it/500/666" />
+            <CardContent
+              sx={{
+                fontWeight: "800",
+                fontSize: "1rem",
+                textAlign: "center",
+              }}
+            >
+              <Typography
+                variant="h6"
+                component="div"
+                sx={{ fontWeight: "bold" }}
               >
-                <div>LEMON BLUEBERRY CAKE</div>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </Grid>
-        <Grid item container justifyContent="center" xl={3} lg={4} md={6}>
-          <Card sx={{ maxWidth: 500, borderRadius: "12px" }}>
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                image="https://place-hold.it/500/666"
-              />
-
-              <CardContent
-                sx={{
-                  fontWeight: "800",
-                  fontSize: "1rem",
-                  textAlign: "center",
-                }}
+                CARROT CAKE
+              </Typography>
+              <Typography variant="body2" component="div">
+                Flavorful and moist cake with a hint of spice, topped with
+                creamy frosting.
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Grid>
+      <Grid item>
+        <Card sx={{ maxWidth: 500, borderRadius: "12px" }}>
+          <CardActionArea>
+            <CardMedia component="img" image="https://place-hold.it/500/666" />
+            <CardContent
+              sx={{
+                fontWeight: "800",
+                fontSize: "1rem",
+                textAlign: "center",
+              }}
+            >
+              <Typography
+                variant="h6"
+                component="div"
+                sx={{ fontWeight: "bold" }}
               >
-                <div>LEMON BLUEBERRY CAKE</div>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </Grid>
-        <Grid item container justifyContent="center" xl={3} lg={4} md={6}>
-          <Card sx={{ maxWidth: 500, borderRadius: "12px" }}>
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                image="https://place-hold.it/500/666"
-              />
-
-              <CardContent
-                sx={{
-                  fontWeight: "800",
-                  fontSize: "1rem",
-                  textAlign: "center",
-                }}
+                CHOCOLATE CAKE
+              </Typography>
+              <Typography variant="body2" component="div">
+                Indulge in rich, velvety layers of cocoa goodness.
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Grid>
+      <Grid item>
+        <Card sx={{ maxWidth: 500, borderRadius: "12px" }}>
+          <CardActionArea>
+            <CardMedia component="img" image="https://place-hold.it/500/666" />
+            <CardContent
+              sx={{
+                fontWeight: "800",
+                fontSize: "1rem",
+                textAlign: "center",
+              }}
+            >
+              <Typography
+                variant="h6"
+                component="div"
+                sx={{ fontWeight: "bold" }}
               >
-                <div>LEMON BLUEBERRY CAKE</div>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </Grid>
+                RED VELVET CAKE
+              </Typography>
+              <Typography variant="body2" component="div">
+                A cocoa-infused, scarlet sensation with creamy frosting.
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
       </Grid>
     </Grid>
   );
