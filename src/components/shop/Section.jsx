a439579df50accc67f92eb45bc25e74cf0aee0aa
Anthony Viera <anthonyviera4@gmail.com>
2023-10-28 22:21:03 -0500

    Good

--- src/components/shop/Section.jsx
+++ src/components/shop/Section.jsx
@@ -1,37 +1,102 @@
 import React from "react";
-import { Typography } from "@mui/material";
+import { Hidden, Typography } from "@mui/material";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import { CardActionArea } from "@mui/material";
 import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
 
 export default function Section({ title, description, items }) {
   return (
     <Grid item container spacing={2}>
-      <Grid item md={2} sm={12}>
+      <Grid item md={2} sm={12} xxl={12}>
         <Typography
           variant="h4"
           fontWeight={1000}
           sx={{
             paddingBottom: "8px",
             paddingTop: "24px",
-            textAlign: { md: "left", xs: "center" },
+            textAlign: { md: "left", xs: "center", xxl: "center" },
           }}
         >
           {title.toUpperCase()}
         </Typography>
-        <Typography sx={{ textAlign: { md: "left", xs: "center" } }}>
+        <Typography
+          sx={{ textAlign: { md: "left", xs: "center", xxl: "center" } }}
+        >
           {description}
         </Typography>
       </Grid>
+      <Box
+        sx={{
+          overflowX: "auto",
+          display: { xs: "none", xxl: "flex" },
+          width: "100%",
+          flex: "1 0 auto",
+          "&::-webkit-scrollbar": {
+            height: "12px",
+          },
+          "&::-webkit-scrollbar-track": {
+            backgroundColor: "#807E75",
+          },
+          "&::-webkit-scrollbar-thumb": {
+            backgroundColor: "#CDCBBC",
+            borderRadius: "20px",
+            border: "3px solid #CDCBBC",
+          },
+        }}
+      >
+        {items?.map((item, index) => (
+          <Card
+            key={index}
+            sx={{
+              borderRadius: "12px",
+              minWidth: "500px",
+              margin: "24px 8px",
+            }}
+          >
+            <CardActionArea>
+              <CardMedia component="img" image={item.image} />
+              <CardContent
+                sx={{
+                  fontWeight: "800",
+                  fontSize: "1rem",
+                  textAlign: "center",
+                }}
+              >
+                <Typography
+                  variant="h6"
+                  component="div"
+                  sx={{ fontWeight: "bold" }}
+                >
+                  {item.title.toUpperCase()}
+                </Typography>
+                <Typography
+                  variant="body2"
+                  component="div"
+                  sx={{
+                    textOverflow: "ellipsis",
+                    overflow: "hidden",
+                    whiteSpace: "nowrap",
+                  }}
+                >
+                  {item.description}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        ))}
+      </Box>
       <Grid
         item
         container
         xs={12}
         md={10}
+        xxl={12}
         spacing={2}
         justifyContent={"center"}
+        sx={{ border: "1px solid black", display: { xxl: "none" } }}
       >
         {items?.map((item, index) => (
           <Grid
@@ -44,7 +109,10 @@ export default function Section({ title, description, items }) {
             justifyContent={"center"}
           >
             <Card
-              sx={{ borderRadius: "12px", maxWidth: "500px", margin: "0 auto" }}
+              sx={{
+                borderRadius: "12px",
+                margin: "0 auto",
+              }}
             >
               <CardActionArea>
                 <CardMedia
--- src/components/shop/Shop.jsx
+++ src/components/shop/Shop.jsx
@@ -8,37 +8,43 @@ export default function Shop() {
       title: "carrot cake",
       description:
         "A delightful, moist dessert bursting with the natural sweetness of carrots, complemented by warm spices and a luscious cream cheese frosting.",
-      image: "https://place-hold.it/500/666",
+      image: "https://place-hold.it/1000/666",
     },
     {
       title: "chocolate cake",
       description:
         "Decadent, moist, and rich in flavor, chocolate cake is a delightful dessert that satisfies with its indulgent cocoa-infused layers and velvety smooth frosting.",
-      image: "https://place-hold.it/500/666",
+      image: "https://place-hold.it/1000/666",
     },
     {
       title: "red velvet cake",
       description:
         "Rich and moist with a hint of cocoa, tinted red, and topped with smooth cream cheese frosting.",
-      image: "https://place-hold.it/500/666",
+      image: "https://place-hold.it/1000/666",
     },
     {
       title: "classic vanilla cake",
       description:
         " A timeless classic with a simple, elegant flavor, perfect for any occasion.",
-      image: "https://place-hold.it/500/666",
+      image: "https://place-hold.it/1000/666",
     },
     {
       title: "galaxy cake",
       description:
         "Embark on a cosmic journey with our stunning cake that's out of this world, with swirls of color and a taste that's truly stellar.",
-      image: "https://place-hold.it/500/666",
+      image: "https://place-hold.it/1000/666",
     },
     {
       title: "strawberry ganache cake",
       description:
         "Indulge in a harmonious blend of rich, velvety chocolate ganache and the vibrant sweetness of fresh strawberries in every slice.",
-      image: "https://place-hold.it/500/666",
+      image: "https://place-hold.it/1000/666",
+    },
+    {
+      title: "lemon blueberry cake",
+      description:
+        "A refreshing, zesty lemon cake with a burst of blueberries in every bite.",
+      image: "https://place-hold.it/1000/666",
     },
   ];
   const cupcakeItems = [
@@ -46,27 +52,27 @@ export default function Shop() {
       title: "vanilla cupcakes",
       description:
         "Made with a moist vanilla cake and topped with a smooth vanilla buttercream frosting.",
-      image: "https://place-hold.it/500/666",
+      image: "https://place-hold.it/1000/666",
     },
     {
       title: "chocolate cupcakes",
       description:
         "Made with a moist chocolate cake and topped with a smooth chocolate buttercream frosting.",
-      image: "https://place-hold.it/500/666",
+      image: "https://place-hold.it/1000/666",
     },
     {
       title: "strawberry cupcakes",
       description:
         "Bursting with fresh, fruity flavor, our strawberry-infused treats are a delightful twist on classic cupcakes.",
-      image: "https://place-hold.it/500/666",
+      image: "https://place-hold.it/1000/666",
     },
   ];
   return (
     <Grid
       container
       sx={{
-        px: { xs: 2, sm: 12, md: 24, xl: 36, xxl: 48 },
-        py: { xs: 6, sm: 12, md: 24 },
+        px: { xs: 2, sm: 12, md: 24, xl: 36, xxl: 0 },
+        py: { xs: 6, sm: 12, md: 24, xxl: 12 },
       }}
       spacing={5}
       justifyContent={"center"}
