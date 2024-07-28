8d4d1f3fb5f4e767abd51ef7ed5e215d8c72509d
Anthony Viera <anthonyviera4@gmail.com>
2024-07-28 17:22:31 -0500

    make it make sense

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -1,10 +1,26 @@
-data "aws_route53_zone" "zone" {
-  name         = var.environment == "prod" ? "paradisecakesbymegan.com" : "dev.paradisecakesbymegan.com"
-  private_zone = false
+resource "aws_route53_zone" "paradise_cakes" {
+  name = var.environment == "prod" ? "paradisecakesbymegan.com" : "dev.paradisecakesbymegan.com"
+}
+
+resource "aws_route53_record" "paradise_cakes" {
+  for_each = {
+    for dvo in aws_acm_certificate.paradise_cakes.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.paradise_cakes.zone_id
 }
 
 resource "aws_route53_record" "paradise_cakes_record" {
-  zone_id = data.aws_route53_zone.zone.zone_id
+  zone_id = aws_route53_zone.paradise_cakes.zone_id
   name    = var.environment == "prod" ? "paradisecakesbymegan.com" : "dev.paradisecakesbymegan.com"
   type    = "A"
   alias {
