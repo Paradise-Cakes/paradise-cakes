ae5ce169d4be0821ad5e1ebae5e24f81ca652bed
Anthony Viera <anthonyviera4@gmail.com>
2024-07-30 22:22:46 -0500

    There its created

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -10,6 +10,12 @@ resource "aws_acm_certificate" "paradise_cakes_dev" {
   validation_method = "DNS"
 }
 
+resource "aws_acm_certificate" "paradise_cakes_dev_api" {
+  count             = var.environment == "prod" ? 0 : 1
+  domain_name       = "dev-api.paradisecakesbymegan.com"
+  validation_method = "DNS"
+}
+
 resource "aws_acm_certificate_validation" "paradise_cakes" {
   count           = var.environment == "prod" ? 1 : 0
   certificate_arn = aws_acm_certificate.paradise_cakes[0].arn
@@ -18,4 +24,9 @@ resource "aws_acm_certificate_validation" "paradise_cakes" {
 resource "aws_acm_certificate_validation" "paradise_cakes_dev" {
   count           = var.environment == "prod" ? 0 : 1
   certificate_arn = aws_acm_certificate.paradise_cakes_dev[0].arn
+}
+
+resource "aws_acm_certificate_validation" "paradise_cakes_dev_api" {
+  count           = var.environment == "prod" ? 0 : 1
+  certificate_arn = aws_acm_certificate.paradise_cakes_dev_api[0].arn
 }
