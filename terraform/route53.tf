d2782ce8e18a87243cce6a4e0beec99961a77919
Anthony Viera <anthonyviera4@gmail.com>
2024-07-30 19:16:21 -0500

    My goodness this looks dum

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -15,9 +15,14 @@ resource "aws_route53_record" "paradise_cakes" {
   }
 }
 
+data "aws_route53_zone" "paradise_cakes" {
+  count = var.environment == "prod" ? 0 : 1
+  name  = "paradisecakesbymegan.com"
+}
+
 resource "aws_route53_record" "paradise_cakes_dev" {
   count   = var.environment == "prod" ? 0 : 1
-  zone_id = aws_route53_zone.paradise_cakes[0].zone_id
+  zone_id = data.aws_route53_zone.paradise_cakes[0].zone_id
   name    = "dev.paradisecakesbymegan.com"
   type    = "A"
   alias {
