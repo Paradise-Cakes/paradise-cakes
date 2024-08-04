4f29ecd5e92822fb79ad31b7cccde8aec6041c01
Anthony Viera <anthonyviera4@gmail.com>
2024-08-04 16:45:22 -0500

    i give up

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -6,5 +6,4 @@ resource "aws_acm_certificate" "paradise_cakes" {
 resource "aws_acm_certificate_validation" "paradise_cakes" {
   certificate_arn         = aws_acm_certificate.paradise_cakes.arn
   validation_record_fqdns = [for record in aws_route53_record.paradise_cakes : record.fqdn]
-  depends_on              = [aws_iam_role.cross_account_access_dev]
 }
