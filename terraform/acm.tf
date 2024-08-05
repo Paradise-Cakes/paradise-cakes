76967dc235209276b9d06f544e69d4e617f75c18
Anthony Viera <anthonyviera4@gmail.com>
2024-08-04 19:22:56 -0500

    Add A records

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -5,5 +5,5 @@ resource "aws_acm_certificate" "paradise_cakes" {
 
 resource "aws_acm_certificate_validation" "paradise_cakes" {
   certificate_arn         = aws_acm_certificate.paradise_cakes.arn
-  validation_record_fqdns = [for record in aws_route53_record.paradise_cakes : record.fqdn]
+  validation_record_fqdns = [for record in aws_route53_record.paradise_cakes_validation_record : record.fqdn]
 }
