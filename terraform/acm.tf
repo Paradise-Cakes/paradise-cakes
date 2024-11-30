6ac512ac311115b1b6ed221c07d96af10724556b
Anthony Viera <anthonyviera96@gmail.com>
2024-11-29 20:11:11 -0600

    correct domain

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -1,3 +1,3 @@
 data "aws_acm_certificate" "paradise_cakes" {
-  domain = var.environment == "prod" ? "megsparadisecakes.com" : "dev.megsparadisecakes.com"
+  domain = var.environment == "prod" ? "www.megsparadisecakes.com" : "www.dev.megsparadisecakes.com"
 }
