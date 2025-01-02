d057a6777aff2f88848854a96a3feda004b051d0
Anthony Viera <anthonyviera4@gmail.com>
2025-01-01 20:03:40 -0600

    nvm

--- src/amplifyConfig.js
+++ src/amplifyConfig.js
@@ -1,10 +1,5 @@
 import { Amplify } from "aws-amplify";
 
-console.log("HELLO")
-console.log(import.meta.env.VITE_DEPLOY_ENV);
-console.log(import.meta.env.VITE_COGNITO_USER_POOL_ID);
-console.log(import.meta.env.VITE_COGNITO_APP_CLIENT_ID);
-
 Amplify.configure({
   Auth: {
     Cognito: {
