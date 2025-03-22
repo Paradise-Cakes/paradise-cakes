aa8bb3d19f2d8b25387775948b3b5b4911d2e2ca
Anthony Viera <anthonyviera4@gmail.com>
2025-03-22 01:36:57 -0500

    hot links

--- src/components/admin/AdminDashboard.jsx
+++ src/components/admin/AdminDashboard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Container, Breadcrumbs, Box, Link, Typography } from "@mui/material";
+import { Container, Box, Typography, Button } from "@mui/material";
+import { Link } from "react-router-dom";
 
 export default function AdminDashboard() {
   return (
@@ -24,6 +25,28 @@ export default function AdminDashboard() {
       >
         Welcome to the Admin Dashboard
       </Typography>
+      <Box sx={{ maxWidth: "300px", margin: "0 auto" }}>
+        <Button
+          fullWidth
+          variant="contained"
+          color="info"
+          component={Link}
+          to="/admin/desserts/create"
+          sx={{ marginTop: "1.25rem", fontSize: "1.25rem", color: "black" }}
+        >
+          Add Dessert
+        </Button>
+        <Button
+          fullWidth
+          variant="contained"
+          color="success"
+          component={Link}
+          to="/admin/desserts"
+          sx={{ marginTop: "1.25rem", fontSize: "1.25rem" }}
+        >
+          View My Desserts
+        </Button>
+      </Box>
     </Container>
   );
 }
